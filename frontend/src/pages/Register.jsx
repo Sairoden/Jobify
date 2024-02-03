3efbbcb97263eb2a25b9a992187800cbe75a8dba
Sairoden <odingandarosa@gmail.com>
2024-02-03 17:48:38 +0800

    Add FormRow Component and Finish Register Page

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,13 +1,102 @@
 // REACT & LIBRARIES
 import { Link } from "react-router-dom";
 
+// STYLES
+import styled from "styled-components";
+
+// UI COMPONENTS
+import { Logo, FormRow } from "../ui";
+
 function Register() {
   return (
-    <div>
-      <h1>Register Page</h1>
-      <Link to="/login">Login</Link>
-    </div>
+    <StyledRegister>
+      <form className="form">
+        <Logo />
+        <h4>Register</h4>
+
+        <FormRow
+          type="text"
+          id="firstName"
+          name="firstName"
+          labelText="First Name"
+          defaultValue="Sairoden"
+        />
+
+        <FormRow
+          type="text"
+          id="lastName"
+          name="lastName"
+          labelText="Last Name"
+          defaultValue="Gandarosa"
+        />
+
+        <FormRow
+          type="text"
+          id="location"
+          name="location"
+          labelText="Location"
+          defaultValue="871 M Delafuente"
+        />
+
+        <FormRow
+          type="email"
+          id="email"
+          name="email"
+          labelText="Email"
+          defaultValue="odingandarosa@gmail.com"
+        />
+
+        <button type="submit" className="btn btn-block">
+          submit
+        </button>
+
+        <p>
+          Already a member?
+          <Link to="/login" className="member-btn">
+            Login
+          </Link>
+        </p>
+      </form>
+    </StyledRegister>
   );
 }
+
+const StyledRegister = styled.section`
+  min-height: 100vh;
+  display: grid;
+  align-items: center;
+
+  .logo {
+    display: block;
+    margin: 0 auto;
+    align-items: center;
+  }
+
+  .form {
+    max-width: 400px;
+    border-top: 5px solid var(--primary-500);
+  }
+
+  h4 {
+    text-align: center;
+    margin-bottom: 1.38rem;
+  }
+
+  p {
+    margin-top: 1rem;
+    text-align: center;
+    line-height: 1.5;
+  }
+
+  .btn {
+    margin-top: 1rem;
+  }
+
+  .member-btn {
+    color: var(--primary-500);
+    letter-spacing: var(--letter-spacing);
+    margin-left: 0.25rem;
+  }
+`;
 
 export default Register;
---
+++ frontend/src/ui/FormRow.jsx
@@ -0,0 +1,19 @@
+function FormRow({ type, name, labelText, defaultValue, required }) {
+  return (
+    <div className="form-row">
+      <label htmlFor="name" className="form-label">
+        {labelText || name}
+      </label>
+      <input
+        type={type}
+        id={name}
+        name={name}
+        className="form-input"
+        defaultValue={defaultValue}
+        required={required}
+      />
+    </div>
+  );
+}
+
+export default FormRow;
