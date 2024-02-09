000e5a96ef94a9b482ad78ae4dac4a0c7d6eb2ba
Sairoden <odingandarosa@gmail.com>
2024-02-09 16:03:29 +0800

    Style ThemeToggle

--- frontend/src/ui/ThemeToggle.jsx
+++ frontend/src/ui/ThemeToggle.jsx
@@ -9,14 +9,29 @@ function ThemeToggle() {
   const { isDarkTheme, toggleDarkTheme } = useDashboardContext();
 
   return (
-    <div onClick={toggleDarkTheme}>
+    <StyledThemeToggle onClick={toggleDarkTheme}>
       {isDarkTheme ? (
         <BsFillSunFill className="toggle-icon" />
       ) : (
         <BsFillMoonFill className="toggle-icon" />
       )}
-    </div>
+    </StyledThemeToggle>
   );
 }
+
+const StyledThemeToggle = styled.button`
+  background: transparent;
+  border-color: transparent;
+  width: 3.5rem;
+  height: 2rem;
+  display: grid;
+  place-items: center;
+  cursor: pointer;
+
+  .toggle-icon {
+    font-size: 1.15rem;
+    color: var(--text-color);
+  }
+`;
 
 export default ThemeToggle;
