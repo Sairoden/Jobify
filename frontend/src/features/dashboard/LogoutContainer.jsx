e2b83de145da517b796253649c78bc34f129f9a6
Sairoden <odingandarosa@gmail.com>
2024-02-09 15:50:46 +0800

    Design LogoutContainer

--- frontend/src/features/dashboard/LogoutContainer.jsx
+++ frontend/src/features/dashboard/LogoutContainer.jsx
@@ -2,6 +2,7 @@
 import { useState } from "react";
 
 // STYLES
+import styled from "styled-components";
 import { FaUserCircle, FaCaretDown } from "react-icons/fa";
 
 // CONTEXTS
@@ -12,7 +13,7 @@ function LogoutContainer() {
   const [showLogout, setShowLogout] = useState(false);
 
   return (
-    <div>
+    <StyledLogoutContainer>
       <button
         type="button"
         className="btn logout-btn"
@@ -30,8 +31,54 @@ function LogoutContainer() {
           Logout
         </button>
       </div>
-    </div>
+    </StyledLogoutContainer>
   );
 }
+
+const StyledLogoutContainer = styled.div`
+  position: relative;
+
+  .logout-btn {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 0 0.5rem;
+  }
+
+  .img {
+    width: 25px;
+    height: 25px;
+    border-radius: 50%;
+  }
+
+  .dropdown {
+    position: absolute;
+    top: 45px;
+    left: 0;
+    width: 100%;
+    box-shadow: var(--shadow-2);
+    text-align: center;
+    visibility: hidden;
+    border-radius: var(--border-radius);
+    background: var(--primary-500);
+  }
+
+  .show-dropdown {
+    visibility: visible;
+  }
+
+  .dropdown-btn {
+    border-radius: var(--border-radius);
+    padding: 0.5rem;
+    background: transparent;
+    border-color: transparent;
+    color: var(--white);
+    letter-spacing: var(--letter-spacing);
+    text-transform: capitalize;
+    cursor: pointer;
+    width: 100%;
+    height: 100%;
+  }
+`;
 
 export default LogoutContainer;
