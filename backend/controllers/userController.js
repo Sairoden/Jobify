c20521b9dee565a168a53aa59b5981eddd009427
Sairoden <odingandarosa@gmail.com>
2024-02-14 16:03:35 +0800

    Register functionality"

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,10 @@
+// MODELS
+import { userModel } from "../models/index.js";
+
 const register = async (req, res) => {
-  return res.status(200).send("register");
+  const user = await userModel.create(req.body);
+
+  return res.status(201).send({ user });
 };
 
 const login = async (req, res) => {
