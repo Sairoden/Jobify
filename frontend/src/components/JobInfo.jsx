e4cf2061e7467123973cc0b1e9abcf575b192f61
Sairoden <odingandarosa@gmail.com>
2024-02-24 18:10:29 +0800

    Implement JobInfo CSS

--- frontend/src/components/JobInfo.jsx
+++ frontend/src/components/JobInfo.jsx
@@ -3,11 +3,32 @@ import styled from "styled-components";
 
 function JobInfo({ icon, text }) {
   return (
-    <div>
+    <StyledJobInfo>
       <span className="job-icon">{icon}</span>
       <span className="job-text">{text}</span>
-    </div>
+    </StyledJobInfo>
   );
 }
+
+const StyledJobInfo = styled.div`
+  display: flex;
+  align-items: center;
+
+  .job-icon {
+    font-size: 1rem;
+    margin-right: 1rem;
+    display: flex;
+    align-items: center;
+
+    svg {
+      color: var(--text-secondary-color);
+    }
+  }
+
+  .job-text {
+    text-transform: capitalize;
+    letter-spacing: var(--letter-spacing);
+  }
+`;
 
 export default JobInfo;
